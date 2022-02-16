6b3322e8e25a584a40ca486045e443e35880cd8e
pcenizapena <p.cenizapena@gmail.com>
2022-02-16 18:26:19 +0100

    cv

---
+++ Home/cv.js
@@ -0,0 +1,6 @@
+var showshow = document.getElementById("introYo")
+showshow.addEventListener("click", saberMas)
+ function saberMas() {
+     document.getElementById("todaInfo").classList.toggle("oculto")
+     document.getElementById("introYo").classList.toggle("oculto")
+ }
